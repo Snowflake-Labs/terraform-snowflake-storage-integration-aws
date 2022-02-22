e512c3a4984a1973d70ad3ce1557f777889042ae
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-02-22 14:49:59 -0800

    Fix bucket acl

--- s3.tf
+++ s3.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "geff_bucket" {
   bucket = local.s3_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "geff_bucket_acl" {
+  bucket = aws_s3_bucket.geff_bucket.id
   acl    = "private"
 }
 
