1ccb764d7cada4b24d74c9cefd9388b8acbbc5ee
Vijaya Sai Prasanth Kommini <72515998+sfc-gh-pkommini@users.noreply.github.com>
2023-05-24 15:52:44 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.4.4"
+  required_version = ">= 1.4.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.38.0"
+      version = ">= 4.38.0"
     }
 
     snowflake = {
